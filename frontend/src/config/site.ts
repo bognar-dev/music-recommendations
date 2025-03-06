2b2c5d6713a0a9be640541919e7c9fea94a2f84d
Niklas Bognar <niklas.bognar@gmail.com>
2025-03-06 13:17:45 +0000

    added og

--- frontend/src/config/site.ts
+++ frontend/src/config/site.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
     name: "Album Covers and Music Recommendations",
-    url: "https://albumcoversurvey.vercel.app",
-    ogImage: "https://albumcoversurvey.vercel.app/og.jpg",
+    url: "https://music-recommendations-sigma.vercel.app/",
+    ogImage: "https://music-recommendations-sigma.vercel.app/og.png",
     description:
       "Find out if album covers can be used to make music recommendations better for you!",
     links: {
